e41591fda037f374246b10ad14aebb46fb501c56
nanliyu <nancarpli@163.com>
2017-10-22 10:17:29 +0800

    1

---
+++ src/main/java/org/seckill/dao/SeckillDao.java
@@ -0,0 +1,18 @@
+package org.seckill.dao;
+
+import org.apache.ibatis.annotations.Param;
+import org.seckill.entity.Seckill;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by nanca on 10/21/2017.
+ */
+public interface SeckillDao {
+    int reduceNumber(@Param("seckillId") long seckillId,@Param("killTime") Date killTime);
+
+    Seckill queryById(long seckillId);
+
+    List<Seckill> queryAll(@Param("offset") int offset, @Param("limit") int limit);
+}
